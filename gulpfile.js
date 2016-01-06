e774e79eb6fc46f64e03358886df7f1d8ee7cb09
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-01-06 20:16:57 +0530

    Initial repo for chirper

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var through2 = require('through2');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+
+gulp.task('browserify', function(){
+
+	gulp.src('./src/main.js')
+		.pipe(plumber())
+		.pipe(through2.obj(function (file, enc, next){
+			browserify(file.path, {'debug': true})
+				.transform('reactify')
+				.bundle(function(err, res){
+					file.contents = res;
+					next(null, file);
+				})
+		}))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('public'))
+});
+
+gulp.task('default', ['browserify']);
+
+gulp.task('watch', function(){
+	gulp.watch('src/**/*.*', ['default']);
+});
